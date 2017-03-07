c75ee1bc2189f3ef90bafb12aff2371467e9d69a
markeastman <meastman@redhat.com>
2017-03-07 13:59:48 +0000

    Added file handling to the application

--- src/main/java/uk/me/eastmans/openshift/files/FilesController.java
+++ src/main/java/uk/me/eastmans/openshift/files/FilesController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * Created by markeastman on 06/03/2017.
@@ -13,11 +16,33 @@ import java.io.File;
 public class FilesController {
 
     private String localFilePath = ".";
+    private static String NEW_FILE_NAME = "modified_via_openshift.txt";
 
     @RequestMapping("/list")
     public File[] listFiles() {
         // We need to get a list of files and return to the display
         File current = new File(localFilePath);
         return current.listFiles();
+    }
+
+    @RequestMapping("/update")
+    public String updateFile()
+    {
+        File current = new File(localFilePath);
+        File file = new File(current,NEW_FILE_NAME);
+        boolean exists = file.exists();
+        String msg = "Unable to process file operations";
+        try
+        {
+            FileWriter wr = new FileWriter(file);
+            wr.write( "Modified at " + new Date() );
+            msg = "File " + file.getName() + " has been " + (exists ? "updated" : "created");
+            wr.close();
+        }
+        catch (IOException exception)
+        {
+            msg = "Unable to write file " + exception.toString();
+        }
+        return msg;
     }
 }
